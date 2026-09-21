e3b7b5015aace861b65ba4c9d56d469c09918ba9
Bushra-Rehman <bushra.mtechinternee@gmail.com>
2026-09-21 14:37:20 +0500

    Add Unit Converter page with SQLite storage

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/UnitConverter.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/UnitConverter.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+45fcbf21b15b04c67d198505ed26607127337c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitConverter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
